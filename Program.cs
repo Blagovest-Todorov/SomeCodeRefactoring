e2b6e881b627895150dd8487d3ad9f3b331e0e61
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-20 22:58:26 +0200

    add Program.cs and RefactorThisCode.txt files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int sumDigits = 0;
+            int originNum = 0;
+            bool isSpecialNum = false;
+
+            for (int num = 1; num <= inputNum; num++)
+            {
+                originNum = num;
+
+                while (num > 0)
+                {
+                    sumDigits += num % 10;
+                    num = num / 10;
+                }
+
+                isSpecialNum = (sumDigits == 5) || (sumDigits == 7) || (sumDigits == 11);
+                Console.WriteLine("{0} -> {1}", originNum, isSpecialNum);
+                sumDigits = 0;
+              //  num = originNum;
+            }
+
+        }
+    }
+}
